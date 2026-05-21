5adaaef852c414866cbd7af7d088b1b179c76435
Wolgrand Neto <wolgrand@live.com>
2026-05-21 14:56:21 +0100

    Add log category endpoint for item purchases

--- js/config.js
+++ js/config.js
@@ -5,6 +5,7 @@ const CONFIG = {
     TORN_API_BASE: 'https://api.torn.com',
     MARKET_API_ENDPOINT: '/market/bazaar?selections=itemids',
     EVENTS_ENDPOINT: '/user/?selections=events',
+    LOG_ENDPOINT: '/user/?selections=log&category=items',
     PRICE_CACHE_DURATION: 60 * 60 * 1000, // 1 hour in milliseconds
     STORAGE_KEYS: {
         API_KEY: 'torn_api_key',
