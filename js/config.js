61f4332dd373ffe2984d89abc95f57cb114c4cd5
Wolgrand Neto <wolgrand@live.com>
2026-06-11 10:11:55 +0100

    Update EVENTS_ENDPOINT for user event logging

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@
 const CONFIG = {
     TORN_API_BASE: 'https://api.torn.com',
     MARKET_API_ENDPOINT: '/market/bazaar?selections=itemids',
-    EVENTS_ENDPOINT: '/user/?selections=events',
+    EVENTS_ENDPOINT: '/user/log?cat=87&limit=100',
     LOG_ENDPOINT: '/user/log?cat=87&limit=100',
     PRICE_CACHE_DURATION: 60 * 60 * 1000, // 1 hour in milliseconds
     STORAGE_KEYS: {
